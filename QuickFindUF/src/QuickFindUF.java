2e8db5ddaac352bf3ccb8ab4d1a0abae98be93a4
Daniel López <hyperdaniel579exe@gmail.com>
2021-11-02 01:10:44 -0600

    Algorithm for Quick Find.

---
+++ QuickFindUF/src/QuickFindUF.java
@@ -0,0 +1,37 @@
+public class QuickFindUF {
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public void validate(int p) {
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("Index Illegal");
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        validate(p);
+        validate(q);
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        validate(p);
+        validate(q);
+        int pid = id[p];
+        int qid = id[p];
+        if (pid == qid) return;
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+    }
+}
