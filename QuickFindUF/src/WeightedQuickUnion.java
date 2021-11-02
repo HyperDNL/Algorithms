1b036ed7e17b9e14db87d4ba51a39702692f5051
Daniel López <hyperdaniel579exe@gmail.com>
2021-11-02 01:11:37 -0600

    Algorithm for Weighted Quick Union.

---
+++ QuickFindUF/src/WeightedQuickUnion.java
@@ -0,0 +1,46 @@
+public class WeightedQuickUnion {
+    private int[] id;
+    private int[] sz;
+
+    public WeightedQuickUnion(int N) {
+        id = new int[N];
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public void validate(int p) {
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("Index Illegal");
+        }
+    }
+
+    private int root(int i) {
+        validate(i);
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (i == j) return;
+
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] = sz[j];
+        }
+    }
+}
